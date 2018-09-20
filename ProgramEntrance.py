a09fbd076b4ab75807e9c3e8edff79194763a8af
ZhangZhiPku <1801213824@pku.edu.cn>
2018-09-20 15:09:20 +0800

    First Update to ElineVision

--- ProgramEntrance.py
+++ ProgramEntrance.py
@@ -10,6 +10,8 @@ _idx = 0
 _x_dimension = 2
 _plot_resolution = 25
 _extension = 2
+_use_bias = True
+
 np.random.seed(0)
 
 def prepare_data(samples=1000, x_dimension=_x_dimension):
@@ -24,7 +26,7 @@ def prepare_data(samples=1000, x_dimension=_x_dimension):
     return x, y
 
 
-def build_network(input_batch_shape=(_x_dimension, ), n_layers=4):
+def build_network(input_batch_shape=(_x_dimension, ), n_layers=4, use_bias=True):
     model = tf.keras.Sequential()
 
     input_layer = tf.keras.layers.Dense(units=32, activation='linear',
@@ -41,7 +43,7 @@ def build_network(input_batch_shape=(_x_dimension, ), n_layers=4):
     _ = np.array(output_layer.get_weights()).shape
     output_layer.set_weights(np.ones(_))
 
-    layers = [tf.keras.layers.Dense(units=32, activation='tanh', use_bias=False) for i in range(n_layers)]
+    layers = [tf.keras.layers.Dense(units=32, activation='tanh', use_bias=use_bias) for i in range(n_layers)]
 
     model.add(input_layer)
     for _layer in layers:
@@ -82,22 +84,39 @@ if __name__ == '__main__':
     _target_epochs = 12800
     _batch_size = 128
     _verbose_interval = 128
+    _model, _layers = build_network(use_bias=_use_bias)
 
-    _model, _layers = build_network()
-
-    _rxs, _rys = [], []
+    _rxs, _rys, _bxs, _bys = [], [], [], []
     for _layer in _layers:
-        _weights_shape = np.array(_layer.get_weights()).shape
-        _rxs.append(np.random.normal(size=_weights_shape))
-        _rys.append(np.random.normal(size=_weights_shape))
+        if _use_bias:
+            _weights_shape = np.array(_layer.get_weights()[: -1]).shape
+            _rxs.append(np.random.normal(size=_weights_shape))
+            _rys.append(np.random.normal(size=_weights_shape))
+
+            _weights_shape = np.array(_layer.get_weights()[-1]).shape
+            _bxs.append(np.random.normal(size=_weights_shape))
+            _bys.append(np.random.normal(size=_weights_shape))
+        else:
+            _weights_shape = np.array(_layer.get_weights()).shape
+            _rxs.append(np.random.normal(size=_weights_shape))
+            _rys.append(np.random.normal(size=_weights_shape))
+
+            _bxs.append(None)
+            _bys.append(None)
 
     _multipliers = [i / _plot_resolution / 10 for i in range(_plot_resolution + 1)]
     _losses = []
 
     for _x_multiplier in _multipliers:
         for _y_multiplier in _multipliers:
-            for _rx, _ry, _layer in zip(_rxs, _rys, _layers):
-                _layer.set_weights(np.add(_rx * _x_multiplier, _ry * _y_multiplier))
+            for _rx, _ry, _bx, _by, _layer in zip(_rxs, _rys, _bxs, _bys, _layers):
+                if _use_bias:
+                    _layer.set_weights(
+                        np.add(_rx * _x_multiplier, _ry * _y_multiplier) + \
+                        np.add(_bx * _x_multiplier, _by * _y_multiplier)
+                    )
+                else:
+                    _layer.set_weights(np.add(_rx * _x_multiplier, _ry * _y_multiplier))
             _losses.append((_x_multiplier, _y_multiplier, _model.evaluate(x, y)[0]))
 
     df = pd.DataFrame(data=_losses, columns=['X', 'Y', 'Z'])
